6c6518654d773e09271fb1b7f156a020ee270120
hanishbansal231 <108611960+hanishbansal231@users.noreply.github.com>
2023-03-16 18:00:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+let btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () =>{
+    let imageBox = document.querySelector('.imageBox');
+
+    imageBox.classList.add('animated');
+    console.log(imageBox)
+});
+
+
